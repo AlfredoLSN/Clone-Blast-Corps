b0eb5761800be52bedf533f27a3184c3892b2d87
Alfredo Lucas <alfredoliu14@live.com>
2025-11-22 14:13:30 -0300

    feat: integrate scenes

--- path_follow_2d.gd
+++ path_follow_2d.gd
@@ -1,22 +1,20 @@
 extends PathFollow2D
 
-@export var speed = 50.0 # Velocidade constante do caminhão
-var game_over = false
+var speed = 60.0 # Velocidade constante
+var is_game_over = false
 
 func _process(delta):
-	if not game_over:
+	if not is_game_over:
 		progress += speed * delta
 		
-		# Condição de Vitória: Chegou ao final do caminho (ratio 1.0 = 100%)
 		if progress_ratio >= 1.0:
-			print("VITÓRIA! O caminhão escapou.")
-			game_over = true
-			# Chamar tela de vitória aqui
+			print("VITÓRIA! Caminhão salvo.")
+			is_game_over = true
 
-# Conectar o sinal "body_entered" do Area2D (filho deste nó) para detectar colisão
-func _on_area_2d_body_entered(body):
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("obstaculos"):
-		print("GAME OVER! O caminhão bateu.")
-		game_over = true
+		print("DERROTA! Cabum.")
+		is_game_over = true
 		speed = 0
-		# Chamar tela de derrota aqui
